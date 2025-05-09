3f6b1c5ffd55bfc3565c92958baf7ab4702e1c0e
Seulji-jo <joseuling@gmail.com>
2025-05-09 16:07:22 +0900

    add other page

--- js-ts/app.js
+++ js-ts/app.js
@@ -1,5 +1,7 @@
 const ajax = new XMLHttpRequest();
+const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
 ajax.open("GET", NEWS_URL, false);
 ajax.send();
@@ -8,10 +10,30 @@ const newsFeed = JSON.parse(ajax.response);
 
 const $ul = document.createElement("ul");
 
+window.addEventListener("hashchange", function () {
+  const id = location.hash.substring(1);
+
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement("h1");
+
+  title.innerHTML = newsContent.title;
+
+  content.appendChild(title);
+});
+
 newsFeed.map((news) => {
   const $li = document.createElement("li");
-  $li.innerHTML = news.title;
+  const $a = document.createElement("a");
+
+  $a.href = `#${news.id}`; // hash
+  $a.innerHTML = `${news.title} (${news.comments_count})`;
+
+  $li.appendChild($a);
   $ul.appendChild($li);
 });
 
 document.getElementById("root").appendChild($ul);
+document.getElementById("root").appendChild(content);
