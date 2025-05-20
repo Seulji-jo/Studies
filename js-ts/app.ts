b9d5f487d7e9995ce05c48597c7e166b73ee5732
Seulji-jo <joseuling@gmail.com>
2025-05-20 17:03:10 +0900

    풍성한 타입 설정 - readonly

--- js-ts/app.ts
+++ js-ts/app.ts
@@ -4,26 +4,26 @@ interface Store {
 }
 
 interface News {
-  id: number;
-  time_ago: string;
-  title: string;
-  url: string;
-  user: string;
-  content: string;
+  readonly id: number;
+  readonly time_ago: string;
+  readonly title: string;
+  readonly url: string;
+  readonly user: string;
+  readonly content: string;
 }
 
 interface NewsFeed extends News {
-  comments_count: number;
-  points: number;
+  readonly comments_count: number;
+  readonly points: number;
   read?: boolean;
 }
 
 interface NewsDetail extends News {
-  comments: [];
+  readonly comments: [];
 }
 interface NewsComment extends News {
-  comments: [];
-  level: number;
+  readonly comments: [];
+  readonly level: number;
 }
 
 const container: HTMLElement | null = document.getElementById("root");
