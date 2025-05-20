cf77b42689c37bbf61063336ce99c2e149ef131e
Seulji-jo <joseuling@gmail.com>
2025-05-20 17:01:21 +0900

    typeAlias -> interface

--- js-ts/app.ts
+++ js-ts/app.ts
@@ -1,30 +1,30 @@
-type Store = {
+interface Store {
   currPage: number;
   feeds: NewsFeed[];
-};
+}
 
-type News = {
+interface News {
   id: number;
   time_ago: string;
   title: string;
   url: string;
   user: string;
   content: string;
-};
+}
 
-type NewsFeed = News & {
+interface NewsFeed extends News {
   comments_count: number;
   points: number;
   read?: boolean;
-};
+}
 
-type NewsDetail = News & {
+interface NewsDetail extends News {
   comments: [];
-};
-type NewsComment = News & {
+}
+interface NewsComment extends News {
   comments: [];
   level: number;
-};
+}
 
 const container: HTMLElement | null = document.getElementById("root");
 const ajax: XMLHttpRequest = new XMLHttpRequest();
